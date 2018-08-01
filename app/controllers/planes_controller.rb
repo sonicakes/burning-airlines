c1a9295d87b764fbb53e5dc5b548d2e4523b6341
Sherine <sherinemay79@gmail.com>
2018-08-02 09:36:09 +1000

    added some plane

--- app/controllers/planes_controller.rb
+++ app/controllers/planes_controller.rb
@@ -10,6 +10,7 @@ class PlanesController < ApplicationController
   # GET /planes/1
   # GET /planes/1.json
   def show
+    
   end
 
   # GET /planes/new
--- db/schema.rb
+++ db/schema.rb
@@ -44,7 +44,6 @@ ActiveRecord::Schema.define(version: 2018_08_01_015443) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.text "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin"
